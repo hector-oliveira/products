5e4e823b266bc1eb8383342acd909209a3286f7b
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-05 15:14:18 -0300

    feat: app screens

---
+++ src/screens/app/Products/Products.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './Products.styled';
+
+export const Products = () => {
+  return (
+    <S.Container>
+      <S.Text>Products</S.Text>
+    </S.Container>
+  );
+};
