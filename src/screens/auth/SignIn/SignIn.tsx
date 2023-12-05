e3df2a600d8623e26ba376e2d499921fae219183
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-05 14:41:02 -0300

    feat: auth screens

---
+++ src/screens/auth/SignIn/SignIn.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './SignIn.styled';
+
+export const SignIn = () => {
+  return (
+    <S.Container>
+      <S.Text>SignIn</S.Text>
+    </S.Container>
+  );
+};
