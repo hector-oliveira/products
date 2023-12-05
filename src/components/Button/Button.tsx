05f4a7593cee36074923aeb40b53daaf6e9783dd
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-05 14:16:14 -0300

    feat: first files created

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './Button.styled';
+
+export const Button = () => {
+  return (
+    <S.Container>
+      <S.Text>Button</S.Text>
+    </S.Container>
+  );
+};
---
+++ src/utils/themes/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  color: {
+    status_bar: '#032031',
+    secondary: '#6943E0',
+    highlight: '#E0D243',
+    contour: '#8B8758',
+    background: '#011627',
+
+    light_900: '#FFFFFF',
+    light_800: '#F5F5F5',
+
+    dark_900: '#000000',
+    dark_800: '#161616',
+  },
+
+  font: {}
+
+};
