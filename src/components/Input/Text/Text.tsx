30f59d59182e6dde47cf30bce0065f7147f62a07
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-06 10:33:12 -0300

    feat: custom text input

--- src/components/Input/Text/Text.tsx
+++ src/components/Input/Text/Text.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import * as S from './Text.styled';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { theme } from '@themes/theme';
+import { InputProps } from '@interfaces/InputProps';
 
-export const Text = () => {
+export const Text = ({ placeholder, ...rest }: InputProps) => {
   return (
-    <S.Container>
-      <S.Text>Text</S.Text>
+    <S.Container {...rest}>
+      <FontAwesome name='user' size={28} color={ theme.colors.light[800] }/>
+      <S.ContentInput placeholder={placeholder} />     
     </S.Container>
   );
 };
