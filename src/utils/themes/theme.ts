db184157b818c26df2aa379c5c065e8d03a8bffa
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-05 23:41:53 -0300

    feat: theme object receiving other objects to compose the application theme

--- src/utils/themes/theme.ts
+++ src/utils/themes/theme.ts
@@ -1,18 +1,11 @@
+import { borderRadius } from './@BorderRadius';
+import { colors } from './@Colors';
+import { fonts } from './@Fonts';
+import { sizes } from './@Sizes';
+
 export const theme = {
-  color: {
-    status_bar: '#032031',
-    secondary: '#6943E0',
-    highlight: '#E0D243',
-    contour: '#8B8758',
-    background: '#011627',
-
-    light_900: '#FFFFFF',
-    light_800: '#F5F5F5',
-
-    dark_900: '#000000',
-    dark_800: '#161616',
-  },
-
-  font: {}
-
+  borderRadius,
+  colors,
+  fonts,
+  sizes
 };
