785b81141f18cd943d9b0876c6862ee4e7bff032
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-06 10:32:37 -0300

    feat: custom password input

--- src/components/Input/Password/Password.styled.ts
+++ src/components/Input/Password/Password.styled.ts
@@ -1,14 +1,26 @@
 import { theme } from '@themes/theme';
 import styled from 'styled-components/native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-export const Container = styled.View`
-  flex: 1;
+export const Container = styled.SafeAreaView`
+  height: 50px;
   width: 100%;
-  justify-content: center;
+  flex-direction: row;
   align-items: center;
 `;
 
-export const Text = styled.Text`
-  font-size: 24px;
-  color: #000;
+export const ContentInput = styled.TextInput.attrs({
+  placeholderTextColor: theme.colors.light[800],
+})`
+  flex: 1;
+  margin-left: 10px;
+  border-bottom-width: 1px;
+  border-bottom-color: ${theme.colors.light[800]};
+  font-size: ${theme.sizes.default};
+  color: ${ theme.colors.light[800] };
+`;
+
+export const Icon = styled(FontAwesome)`
+  border-bottom-width: 1px;
+  border-bottom-color: ${theme.colors.light[800]};
 `;
--- src/components/Input/Password/Password.tsx
+++ src/components/Input/Password/Password.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './Password.styled';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { theme } from '@themes/theme';
+import { InputProps } from '@interfaces/InputProps';
+import useToggle from '@hooks/useToggle';
 
-export const Password = () => {
-  return (
+export const Password = ({ placeholder, secureTextEntry, ...rest }: InputProps) => {
+  
+  const { value, toggle } = useToggle(!secureTextEntry);
+  
+  return(
     <S.Container>
-      <S.Text>Password</S.Text>
+      <FontAwesome name='lock' size={28} color={ theme.colors.light[800] }/>
+      <S.ContentInput
+        {...rest}
+        placeholder={placeholder}
+        secureTextEntry={value}
+      />
+      <S.Icon
+        name={value ? 'eye' : 'eye-slash'}
+        size={28}
+        color={ theme.colors.light[800] }
+        onPress={toggle}
+
+      />
     </S.Container>
   );
 };
