ee404e9f70d099a5338b5c4d2c0f0ea7bc893b16
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-05 23:40:08 -0300

    styles: setting styled Welcome Screen

--- src/screens/auth/Welcome/Welcome.styled.ts
+++ src/screens/auth/Welcome/Welcome.styled.ts
@@ -6,9 +6,10 @@ export const Container = styled.View`
   width: 100%;
   justify-content: center;
   align-items: center;
+  background-color: ${theme.colors.background};
 `;
 
 export const Text = styled.Text`
   font-size: 24px;
-  color: #000;
+  color: ${ theme.colors.light[900] };
 `;
