e036509244f93f3d59655cd8b2256aa48e2b798b
Hector Abreu de Oliveira <hectorabreu_oliveira@hotmail.com>
2023-12-06 10:34:55 -0300

    feat: assembling the welcome screen

--- src/screens/auth/Welcome/Welcome.styled.ts
+++ src/screens/auth/Welcome/Welcome.styled.ts
@@ -4,12 +4,16 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   width: 100%;
-  justify-content: center;
-  align-items: center;
   background-color: ${theme.colors.background};
 `;
 
-export const Text = styled.Text`
-  font-size: 24px;
+export const Title = styled.Text`
+  font-size: ${ theme.sizes.lg };
   color: ${ theme.colors.light[900] };
+  text-align: center;
+`;
+
+export const ContainerInput = styled.SafeAreaView`
+  width: 80%;
+  align-self: center;
 `;
--- src/screens/auth/Welcome/Welcome.tsx
+++ src/screens/auth/Welcome/Welcome.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import * as S from './Welcome.styled';
+import { Input } from '@components/all-components';
+import { theme } from '@themes/theme';
 
 export const Welcome = () => {
   return (
     <S.Container>
-      <S.Text>Welcome</S.Text>
+      <S.Title>Welcome</S.Title>
+      <S.ContainerInput>
+        <Input.Text placeholder='Nome' />
+        <Input.Password placeholder='Senha' autoCapitalize='none' />
+      </S.ContainerInput>
     </S.Container>
   );
 };
